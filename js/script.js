8f946ba83405be8d23575baec3ea4ac7a8b90e40
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-06 15:34:06 +0200

    Added a <div> in the container with append and added class

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
 //inizializzazione variabile
 let output;
+//Chiamata container
+const container = document.querySelector(".container");
 
 for (let i = 1; i <=100; i++){
     if(i % 3 === 0 && i % 5 === 0) { //controllo multipli di 3 e 5
@@ -13,5 +15,27 @@ for (let i = 1; i <=100; i++){
     } else {
         output = i;
     }
+
+    //Stampa numeri nella console
     console.log(output);
+
+    /* Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare. */
+    let square = document.createElement("div");
+
+    square.append(output);
+
+    container.appendChild(square);
+    //Diamo a tutti gli square una classe square
+    square.classList.add("square");
+    //aggiungiamo una classe a square in base al numero di cui è multiplo
+    if (output === i) {
+        square.classList.add("not-divisible");
+    } else if (output === "FizzBuzz") {
+        square.classList.add("divisible-by-3-5");
+    } else if (output === "Buzz") {
+        square.classList.add("divisible-by-5");
+    } else if (output === "Fizz") {
+        square.classList.add("divisible-by-3");
+    }
+    
 }
